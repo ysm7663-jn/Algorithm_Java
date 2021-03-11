e2fc3c4d2af8f51bb101df36ddef4ea10f196a6e
YunSeongMin <ysm7663@gmail.com>
2021-03-11 23:41:48 +0900

    2021-03-11 N14888 연산자 끼워넣기

---
+++ src/Other/N14888.java
@@ -0,0 +1,51 @@
+package Other;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 연산자 끼워넣기 (윤석이 방법)
+public class N14888 {
+
+	static int Min = 1000000001;
+	static int Max = -1000000001; 
+	static int[] num = new int[11];
+	static int[] oper = new int[4];
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		// 수 입력
+		for (int i = 0; i < N; i++) 
+			num[i] = Integer.parseInt(br.readLine());
+		
+		// 연산자 갯수 입력
+		for (int i = 0; i < oper.length; i++) 
+			oper[i] = Integer.parseInt(br.readLine());
+			
+		Calculator(1, oper[0], oper[1], oper[2], oper[3], num[0]);
+		
+		System.out.println(Max);
+		System.out.println(Min);
+		
+	}
+	
+	static void Calculator(int index, int plus, int minus, int mul, int div, int sum) {
+		
+		 if (plus == 0 && minus == 0 && mul == 0 && div == 0) {
+			 Max = Math.max(Max, sum);
+			 Min = Math.min(Min, sum);
+		 } else if (plus < 0 || minus < 0 || mul < 0 || div < 0) { 
+			 return;
+		 } else {
+			 Calculator(index+1, plus-1, minus, mul, div, sum+num[index]);
+			 Calculator(index+1, plus, minus-1, mul, div, sum-num[index]);
+			 Calculator(index+1, plus, minus, mul-1, div, sum*num[index]);
+			 Calculator(index+1, plus, minus, mul, div-1, sum/num[index]);
+		 }
+	}
+
+}
---
+++ src/Other/N14888_2java.java
@@ -0,0 +1,65 @@
+package Other;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 연산자 끼워넣기 
+public class N14888_2java {
+
+	static int Min = 1000000001;
+	static int Max = -1000000001; 
+	static int[] numer;
+	static int[] oper = new int[4];
+	static int N;
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		numer = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		// 수 입력
+		for (int i = 0; i < N; i++) 
+			numer[i] = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine(), " ");
+		// 연산자 갯수 입력
+		for (int i = 0; i < 4; i++) 
+			oper[i] = Integer.parseInt(st.nextToken());
+			
+		Calculator(numer[0], 1);
+		
+		System.out.println(Max);
+		System.out.println(Min);
+		
+	}
+	
+	static void Calculator(int num, int idx) {
+		
+		 if (idx == N) {
+			 Max = Math.max(Max, num);
+			 Min = Math.min(Min, num);
+			 return;
+		 } 
+		 
+		 for (int i = 0; i < 4; i++) {
+			 if(oper[i] > 0) {
+				 oper[i]--;
+				 
+				 switch(i) {
+				 case 0: Calculator(num + numer[idx], idx + 1); break;
+				 case 1: Calculator(num - numer[idx], idx + 1); break;
+				 case 2: Calculator(num * numer[idx], idx + 1); break;
+				 case 3: Calculator(num / numer[idx], idx + 1); break;
+				 }
+				 
+				 oper[i]++;
+			 }
+		 }
+	}
+
+}
