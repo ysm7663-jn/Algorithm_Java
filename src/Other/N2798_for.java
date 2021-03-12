ded9c015276b5b1d8af72a2ae8bb8c241ba04c71
YunSeongMin <ysm7663@gmail.com>
2021-03-12 17:21:18 +0900

    2021-03-12 블랙잭

---
+++ src/Other/N2798_for.java
@@ -0,0 +1,50 @@
+package Other;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 블랙잭
+public class N2798_for {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
+
+		int sum = 0;
+		int max = 0;
+
+		int[] arr = new int[N];
+
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		for (int i = 0; i < arr.length-2; i++) {
+			for (int j = i + 1; j < arr.length-1; j++) {
+				for (int k = j + 1; k < arr.length; k++) {
+					sum += arr[i] + arr[j] + arr[k];
+					
+					if (max < sum && sum <= M) {
+						max = sum;
+						sum = 0;
+					} else {
+						sum = 0;
+					}
+					
+					if (sum > M)
+						break;
+
+				}
+			}
+		}
+		System.out.println(max);
+//		런타임 오류
+	}
+
+}
