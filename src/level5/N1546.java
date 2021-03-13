6c78de7e49913afce16ec3a4a68a1321de68a345
YunSeongMin <ysm7663@gmail.com>
2021-03-13 17:10:29 +0900

    2021-03-13 N1546 평균

--- src/level5/N1546.java
+++ src/level5/N1546.java
@@ -3,6 +3,7 @@ package level5;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class N1546 {
 
@@ -10,25 +11,28 @@ public class N1546 {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int num = Integer.parseInt(br.readLine());
-		double[] arr = new double[num];
-		double total = 0;
-		double max = 0;
+		int N = Integer.parseInt(br.readLine());
 		
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = Double.parseDouble(br.readLine());
+		int max = -1;
+		double sum = 0.0;
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		for (int i = 0; i < N; i++) {
 			
-			if (arr[i] > max) {
-				max = arr[i];
+			int num = Integer.parseInt(st.nextToken());
+			
+			if (num > max) {
+				max = num;
 			}
 			
-			total += arr[i];
+			sum += num;
+			
 		}
 		
-		double avg = total / max * 100 / num;
-		System.out.println(avg);
+		System.out.println(((sum / max) * 100.0) / N);
 		
-		br.close();
+		
 	}
 
 }
