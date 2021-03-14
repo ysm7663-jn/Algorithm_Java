49ff7faa3823422b93c9e82641cbd974dc270c80
YunSeongMin <ysm7663@gmail.com>
2021-03-14 13:33:27 +0900

    2021-03-13 N1546

--- src/level5/N1546.java
+++ src/level5/N1546.java
@@ -5,34 +5,34 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+// ЦђБе
 public class N1546 {
 
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		int N = Integer.parseInt(br.readLine());
-		
+
 		int max = -1;
 		double sum = 0.0;
-		
+
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		
+
 		for (int i = 0; i < N; i++) {
-			
+
 			int num = Integer.parseInt(st.nextToken());
-			
+
 			if (num > max) {
 				max = num;
 			}
-			
+
 			sum += num;
-			
+
 		}
-		
+
 		System.out.println(((sum / max) * 100.0) / N);
-		
-		
+
 	}
 
 }
