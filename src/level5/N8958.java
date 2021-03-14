954d1d09e16f497f09c130430db2a73d97bc9c91
YunSeongMin <ysm7663@gmail.com>
2021-03-14 13:32:41 +0900

    2021-03-13 N8958 OX퀴즈

--- src/level5/N8958.java
+++ src/level5/N8958.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+// OXÄûÁî
 public class N8958 {
 
 	public static void main(String[] args) throws IOException {
