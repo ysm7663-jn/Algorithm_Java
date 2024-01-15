2daedbdc31545afad68f505023c0f09518cfb1fd
ysm7663 <ysm7663@gmail.com>
2024-01-15 13:47:55 +0900

    N10926 ??!

---
+++ src/level1/N10926.java
@@ -0,0 +1,21 @@
+package level1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class N10926 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = sc.next();
+		
+		System.out.println(str + "??!");
+
+	}
+ 
+}
